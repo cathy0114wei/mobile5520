2899fc0b468b4ea4820f5491665e5b0ed23faffc
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2022-01-23 12:00:46 -0800

    first assignment

--- app/src/main/java/com/example/numad22sp_xueningwei/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_xueningwei/MainActivity.java
@@ -3,9 +3,18 @@ package com.example.numad22sp_xueningwei;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void toastMsg(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public void displayToastMsg(View v){
+        toastMsg("Xuening Wei wei.xu@northeastern.edu");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
